0f19ccd64a2ff692c98c3eb83eb4c08e14e5313b
Jelena <jelena.novakovic991@gmail.com>
2021-11-18 12:27:36 +0100

    Close modal form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,24 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birthdateInput = document.getElementById("birthdate");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+function closeForm() {
+  modalbg.style.display = "none";
+}
+
 
 
