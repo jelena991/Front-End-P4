187306ac3b251ba23bdc909b4bb0fa698c52443e
Jelena <jelena.novakovic991@gmail.com>
2021-11-30 15:14:43 +0100

    pokusaj 2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,9 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
+  var y = document.getElementById("overlay");
   if (x.className === "topnav") {
     x.className += " responsive";
+  y.className += " menu";
   } else {
     x.className = "topnav";
   }
@@ -12,12 +14,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-
-
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 
 // launch modal form
 function launchModal() {
@@ -39,17 +37,17 @@ function closeForm() {
 
 // submit modal form
 
-  const forms = document.querySelectorAll('.needs-validation');
-  const successMsg = document.getElementById('success-message');
-  const submitBtn = document.getElementById('btn-submit');
-  const successCloseBtn = document.getElementById('btn-close');
-  const errors = document.querySelectorAll(".error-message");
+const forms = document.querySelectorAll('.needs-validation');
+const successMsg = document.getElementById('success-message');
+const submitBtn = document.getElementById('btn-submit');
+const successCloseBtn = document.getElementById('btn-close');
+const errors = document.querySelectorAll(".error-message");
 
 
 
-  //Loop over the form and prevent submission
+//Loop over the form and prevent submission
 
-  for (let i = 0; i < forms.length; i++){
+for (let i = 0; i < forms.length; i++){
     const form = forms[i];
 
     form.addEventListener('submit', function(event){
@@ -75,9 +73,9 @@ function closeForm() {
 
       form.classList.add('was-validated');
     });
-  }
+}
 
-  function validate(event) {
+function validate(event) {
     const forms = document.querySelectorAll('.needs-validation');
     const successMsg = document.querySelectorAll('.success-message');
 
@@ -96,113 +94,4 @@ function closeForm() {
       }
 
       form.classList.add('was-validated');
-  }
-
-
-
-  
-
-  
-  /*//const formData = new FormData();
-
-  let displaySuccess = true;
-
-  const firstNameInput=document.getElementById("first").value;
-  const lastNameInput=document.getElementById("last").value;
-  const emailInput = document.getElementById("email").value;
-  const dateInput = document.getElementById("birthdate").value;
-  const locationInput = document.getElementsByName("location");
-  const checkboxInput = document.getElementById("checkbox1");
-
-  const firstNameErrorMsg= document.getElementById("error-first");
-  const lastNameErrorMsg= document.getElementById("error-last");
-
-  const emailErrorMsg = document.getElementById("error-email");
-  const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-  const birthdateErrorMsg = document.getElementById("error-date");
-  const locationErrorMsg = document.getElementById("error-location");
-  const checkboxErrorMsg = document.getElementById("error-checkbox");
-
-  //validation msg
-  const validationMsg = document.getElementById("success");
-
-  if (firstNameInput.length < 2){
-    console.log ("User name not long enough"); 
-    event.preventDefault();
-    firstNameErrorMsg.style.display = "inline";
-    displaySuccess = false;
-  }
-
-  console.log("FIRST NAME IS:", firstNameInput);
-
-
-  if (lastNameInput.length < 2){
-    console.log ("Last name not long enough");
-    event.preventDefault();
-    lastNameErrorMsg.style.display = "inline";
-    displaySuccess = false;
-  }
-
-  console.log("LAST NAME IS:", lastNameInput);
-
-  if (!re.test(emailInput) ){
-    console.log ("Not a valid Email");
-    event.preventDefault();
-    emailErrorMsg.style.display = "inline";
-    displaySuccess = false;
-  }
-
-  console.log("EMAIL IS:", emailInput);
-
-  if (!dateInput){
-    console.log ("You must pick a birthdate");
-    event.preventDefault();
-    birthdateErrorMsg.style.display = "inline";
-    displaySuccess = false;
-  }
-
-  console.log("BIRTHDAY IS:", dateInput);
-
-  let radioChecked = false;
-  for (let i = 0; i < locationInput.length ;i++){
-    if (locationInput[i].checked){
-      radioChecked = true;
-      break;
-    } 
-  }
-  if (!radioChecked){
-    console.log ("You must pick a location");
-    event.preventDefault();
-    locationErrorMsg.style.display = "inline";
-    displaySuccess = false;
-  }
-  
-  if (!checkboxInput.checked){
-    console.log ("You must check to agree to terms and conditions.");
-    event.preventDefault();
-    checkboxErrorMsg.style.display = "inline";
-    displaySuccess = false;
-  }
-
-  
-  if(displaySuccess){
-
-    alert("Thank you for submiting your registration details!")
-  }
-}  
-
-
-
-function validate(form) {
-  const input = document.getElementById("first");
-  
-  
-  if (firstNameInput.value < 2){
-    input.setCustomValidity('You gotta fill this out, yo!');
-  } else {
-    input.setCustomValidity(' ');
-  }
-  input.reportValidity();
-
-}*/
+}
